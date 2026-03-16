57293a1b32c770422738988c5684656e0ad558fb
technology <technology@fitistan.com>
2026-03-16 20:39:09 +0530

    new version migration

--- client/src/components/VoiceInterface/index.js
+++
@@ -1,4 +0,0 @@
-export { default as VoiceInterface } from './VoiceInterface';
-export { default as VoiceDashboard } from './VoiceDashboard';
-export { default as EmployeeCard } from './EmployeeCard';
-export { default as AttendanceChart } from './AttendanceChart';
